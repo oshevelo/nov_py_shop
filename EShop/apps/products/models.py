f20f30560ed27c4b40f1f2f8704436f420723d92
Jack_oS <s.panchenko@lookout.com>
2020-01-17 13:43:09 +0200

    price model change

--- EShop/apps/products/models.py
+++ EShop/apps/products/models.py
@@ -5,15 +5,15 @@ class Product(models.Model):
     name = models.CharField(max_length=128, blank=False)
     description = models.TextField(blank=True)
 
-    price = models.DecimalField(decimal_places=2)
+    price = models.FloatField(default=0)
 
     scu = models.CharField(max_length=32) # Stock Keeping Unit, SCU
-    gtin = models.IntegerField(max_length=14)  # Global Trade Item Number, GTIN
+    gtin = models.CharField(max_length=14)  # Global Trade Item Number, GTIN
 
     stock = models.IntegerField(default=0)
     is_available = models.BooleanField(default=False)
 
-    image = models.ImageField()
+    # image = models.ImageField()
 
     # category = models.ForeignKey(Category)
 
