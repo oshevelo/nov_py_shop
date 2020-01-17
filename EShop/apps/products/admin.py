71677a4357ae048c23d782793875b5b1e4bb7341
Jack_oS <s.panchenko@lookout.com>
2020-01-17 16:30:14 +0200

    list_display added

--- EShop/apps/products/admin.py
+++ EShop/apps/products/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .models import Product
 
 
-admin.site.register(Product)
+admin.site.register(Product, list_display=('id', 'name', 'price', 'scu', 'gtin', 'stock', 'is_available'))
