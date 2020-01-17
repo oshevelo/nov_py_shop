1dd181af573ba39b32cdc69c3f04ef66ff7f511b
Jack_oS <s.panchenko@lookout.com>
2020-01-17 13:45:25 +0200

    db default -> postgresql

--- EShop/EShop/settings.py
+++ EShop/EShop/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django.contrib.gis',
+
     'apps.products',
 ]
 
@@ -76,10 +78,19 @@ WSGI_APPLICATION = 'EShop.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'eshop',
+        'USER': 'eshop_user',
+        'PASSWORD': 'eshop',
+        'HOST': 'localhost',
+        'PORT': '',
+    },
 }
 
 
