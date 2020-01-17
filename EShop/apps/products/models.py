3e0b938a4b89b1a779ff7e6dbabe4fa7b4db96d2
Jack_oS <s.panchenko@lookout.com>
2020-01-17 09:54:18 +0200

    model product add

--- EShop/apps/products/models.py
+++ EShop/apps/products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=128, blank=False)
+    description = models.TextField(blank=True)
+
+    price = models.DecimalField(decimal_places=2)
+
+    scu = models.CharField(max_length=32) # Stock Keeping Unit, SCU
+    gtin = models.IntegerField(max_length=14)  # Global Trade Item Number, GTIN
+
+    stock = models.IntegerField(default=0)
+    is_available = models.BooleanField(default=False)
+
+    image = models.ImageField()
+
+    # category = models.ForeignKey(Category)
+
+    def __str__(self):
+        return '{} {}'.format(self.id, self.name)
