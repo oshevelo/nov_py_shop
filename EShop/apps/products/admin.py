69c0c14f9b4cdb74a058dcc29ff184d18e13fff4
Jack_oS <s.panchenko@lookout.com>
2020-01-17 16:35:32 +0200

    list_filter add

--- EShop/apps/products/admin.py
+++ EShop/apps/products/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Product
 
 
-admin.site.register(Product, list_display=('id', 'name', 'price', 'scu', 'gtin', 'stock', 'is_available'))
+admin.site.register(
+    Product,
+    list_display=('id', 'name', 'price', 'scu', 'gtin', 'stock', 'is_available'),
+    list_filter=('is_available',)
+)
