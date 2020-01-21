e406be752bdc5d7ac1fd499c2160875fc0dd3856
yura_skakun <yuraskakun1@gmail.com>
2020-01-21 20:07:12 +0200

    add models & fix requirements

--- EShop/apps/carts/admin.py
+++ EShop/apps/carts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Cart
 
 # Register your models here.
+
+
+admin.site.register(Cart)
+
+
--- EShop/apps/carts/models.py
+++ EShop/apps/carts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import datetime
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cart_create = models.DateTimeField('date created')
+    
+    def __str__(self):
+        return 'pk = {} --- user = {}'.format(self.pk, self.user)
+        
