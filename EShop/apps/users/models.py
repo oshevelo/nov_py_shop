3407532d5fa1a4cea47a8bd87c6cea3af100bb28
Andrii <baranovskyi.and@gmail.com>
2020-01-21 16:27:07 +0200

    add users models

--- EShop/apps/users/models.py
+++ EShop/apps/users/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from phone_field import PhoneField
+
 
 # Create your models here.
+class UserProfile(models.Model):
+    first_name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    patronymic = models.CharField(max_length=50)
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name='profile',
+    )
+
+
+class UserAddress(models.Model):
+    user = models.ForeignKey(
+        UserProfile,
+        on_delete=models.CASCADE,
+    )
+    city = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+
+
+class UserPhone(models.Model):
+    user = models.ForeignKey(
+        UserProfile,
+        on_delete=models.CASCADE,
+    )
+    phone = PhoneField(help_text='Contact phone number')
