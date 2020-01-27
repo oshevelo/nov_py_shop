7524b7afca061270cd8a8bdc51d1a5a344ca0b0e
Denys Maksymenko <denismax8@gmail.com>
2020-01-27 20:53:19 +0200

    Add raw_id field in orders.admin.py

--- EShop/apps/orders/admin.py
+++ EShop/apps/orders/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import Order, OrderItem
 
-admin.site.register(Order)
-admin.site.register(OrderItem)
+
+class OrderAdmin(admin.ModelAdmin):
+    raw_id_fields = ("user",)
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    raw_id_fields = ("order",)
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
