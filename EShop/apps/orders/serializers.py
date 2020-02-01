558f5ab12ec285d0ff5e8b220b0586cf0cab169c
Denys Maksymenko <denismax8@gmail.com>
2020-02-01 14:55:14 +0200

    Cnange orders.serializers

--- EShop/apps/orders/serializers.py
+++ EShop/apps/orders/serializers.py
@@ -1,12 +1,6 @@
 from rest_framework import serializers
 from .models import Order, OrderItem
-
-
-class OrderSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Order
-        fields = ['id',  'pub_id', 'user', 'accepting_time', 'completing_or_rejecting_time', 'status', 'comment']
+from apps.products.serializers import ProductBriefSerializer
 
 
 class OrderBriefSerializer(serializers.ModelSerializer):
@@ -14,13 +8,33 @@ class OrderBriefSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ['id', 'pub_id', 'user', 'accepting_time', 'status']
+        
+
+class OrderItemBriefSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'pub_id', 'product', 'amount']
+        
+
+class OrderSerializer(serializers.ModelSerializer):
+
+    orderitems=OrderItemBriefSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ['id',  'pub_id', 'user', 'accepting_time', 'completing_or_rejecting_time', 'status', 'comment', 'orderitems']
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
 
-    order=OrderBriefSerializer()
+    order=OrderBriefSerializer(read_only=True)
+    product=ProductBriefSerializer(read_only=True)
 
     class Meta:
         model = OrderItem
         fields = ['id', 'pub_id',  'order', 'product', 'amount']
+        
+        
+
 
