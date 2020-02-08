b905608ad8709dbbe093fda7ce523cddaa550142
yura_skakun <yuraskakun1@gmail.com>
2020-02-08 15:30:07 +0200

    add tests and change serializers

--- EShop/apps/carts/serializers.py
+++ EShop/apps/carts/serializers.py
@@ -7,7 +7,7 @@ class CartBriefSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Cart
-        fields = ('public_id', 'updated_at')
+        fields = ('public_id', 'user')
 
 
 class CartItemBriefSerializer(serializers.ModelSerializer):
@@ -21,21 +21,29 @@ class CartItemBriefSerializer(serializers.ModelSerializer):
 
 class CartSerializer(serializers.ModelSerializer):
 
-    items = CartItemBriefSerializer(many=True)
+    items = CartItemBriefSerializer(many=True,read_only=True)
 
     class Meta:
         model = Cart
-        fields = ('public_id', 'user', 'created_at', 'updated_at', 'items')
+        fields = ('id','public_id', 'user', 'created_at', 'updated_at', 'items')
 
 
 class CartItemSerializer(serializers.ModelSerializer):
 
     product = ProductBriefSerializer(read_only=True)
-    cart = CartBriefSerializer()
+#    cart = CartBriefSerializer(read_only=True)
 
     class Meta:
         model = CartItem
         fields = ('id', 'public_id', 'cart', 'product', 'price', 'quantity', 'created_at', 'updated_at')
+
+
+
+
+
+
+
+
 
 
 
