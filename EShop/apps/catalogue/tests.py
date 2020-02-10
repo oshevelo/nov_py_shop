768da05fbeb9ab6a38460b5b8898f64cee825c4a
Мария Маркелова <slimmarkelova@MacBook-Air-Maria.local>
2020-02-10 19:31:32 +0200

    fix tests catalogue

--- EShop/apps/catalogue/tests.py
+++ EShop/apps/catalogue/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
 from apps.catalogue.models import Category
+from apps.catalogue.models import SubCategory
 from rest_framework import status
 
 # Create your tests here.
@@ -8,21 +9,24 @@ from rest_framework import status
 class CatalogueTestsAPI(TestCase):
     
     def setUp(self):
+        self.api = APIClient()
         сat = Category.objects.create(name='test_cat', description='category test')
-        self.c = APIClient()
-
-     def tests_list_category(self):
-        c = APIClient()
-        response = self.c.get('/catalogue/')
-        print(response.status_code)
-        print(response.json())
-
-        
-    def tests_category(self):
-        c = APIClient()
-        response = self.c.get('/catalogue/')
-        print(response.status_code)
-        print(response.json())
-        
+        cat_2 = Category.objects.create(name='test_cat_2', description='category test 2')
+        sub_cat = SubCategory.objects.create(name='test_subcat', description='subcategory test')
+        sub_cat_2 = SubCategory.objects.create(name='test_subcat_2', description='subcategory test 2')
         
 
+    def tests_category_list(self):
+        response = self.api.get('/catalogue/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), [{'id': 1, 'name': 'test_cat', 'description':'category test', 'subcategory':[]},
+                                           {'id': 2, 'name': 'test_cat_2', 'description':'category test 2', 'subcategory':[]}]
+                         )
+    def tests_subcategory_list(self):
+        response = self.api.get('/catalogue/subcategory')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), [{'id': 3, 'name': 'test_subcat', 'description':'subcategory test', 'products':[]},
+                                           {'id': 4, 'name': 'test_subcat_2', 'description':'subcategory test 2', 'products':[]}]
+                         )
+
+        
