353127cff58b31bdf5f0a14ad4abb8f78c175599
Denys Maksymenko <denismax8@gmail.com>
2020-02-17 15:55:34 +0200

    Modify views in orders, add permissions

--- EShop/apps/orders/views.py
+++ EShop/apps/orders/views.py
@@ -1,41 +1,75 @@
 from .models import Order, OrderItem
 from rest_framework import generics
 from rest_framework.pagination import LimitOffsetPagination
-from .serializers import OrderSerializer, OrderItemSerializer, OrderItemBriefSerializer
+from rest_framework.permissions import IsAuthenticated
+from .serializers import OrderSerializer, OrderItemSerializer, OrderCreateUpdateSerializer, OrderItemCreateUpdateSerializer
 from django.shortcuts import get_object_or_404
+from django.http import Http404
 
 
 class OrderList(generics.ListCreateAPIView):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
     pagination_class = LimitOffsetPagination
-
+    
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+        
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return OrderCreateUpdateSerializer
+        return OrderSerializer
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+        
 
 class OrderDetail(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_object(self):
         obj = get_object_or_404(Order, pub_id=self.kwargs.get('order_uuid'))
-        return obj
+        if obj.user==self.request.user:
+            return obj
+        else:
+            raise Http404()
+            
+    def get_serializer_class(self):
+        if self.request.method == 'PUT':
+            return OrderCreateUpdateSerializer
+        return OrderSerializer
         
 
 class OrderItemList(generics.ListCreateAPIView):
-    queryset = OrderItem.objects.all()
     pagination_class = LimitOffsetPagination
+    permission_classes = [IsAuthenticated]
     
     def get_serializer_class(self):
         if self.request.method == 'POST':
-            return OrderItemBriefSerializer
+            return OrderItemCreateUpdateSerializer
         return OrderItemSerializer
+    
+    def get_queryset(self):
+        selected_order = get_object_or_404(Order, pub_id=self.kwargs.get('order_uuid'))
+        if selected_order.user!=self.request.user:
+            raise Http404()
+        return OrderItem.objects.filter(order=selected_order)
+        
+    def perform_create(self, serializer):
+        selected_order=Order.objects.get(pub_id=self.kwargs['order_uuid'])
+        serializer.save(order=selected_order)
 
 
 class OrderItemDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
 
     def get_serializer_class(self):
         if self.request.method == 'PUT':
-            return OrderItemBriefSerializer
+            return OrderItemCreateUpdateSerializer
         return OrderItemSerializer
 
     def get_object(self):
-        obj = get_object_or_404(OrderItem, pub_id=self.kwargs.get('orderitem_uuid'))
+        selected_order = get_object_or_404(Order, pub_id=self.kwargs.get('order_uuid'))
+        if selected_order.user!=self.request.user:
+            raise Http404()
+        obj = get_object_or_404(selected_order.orderitems, pub_id=self.kwargs.get('orderitem_uuid'))
         return obj
