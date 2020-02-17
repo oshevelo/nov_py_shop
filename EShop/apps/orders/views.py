fd024ca9ba05880b3f6037b30a952bba62f48fd3
Denys Maksymenko <denismax8@gmail.com>
2020-02-17 23:14:37 +0200

    Fix views for orders

--- EShop/apps/orders/views.py
+++ EShop/apps/orders/views.py
@@ -4,7 +4,6 @@ from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
 from .serializers import OrderSerializer, OrderItemSerializer, OrderCreateUpdateSerializer, OrderItemCreateUpdateSerializer
 from django.shortcuts import get_object_or_404
-from django.http import Http404
 
 
 class OrderList(generics.ListCreateAPIView):
@@ -27,11 +26,8 @@ class OrderDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated]
 
     def get_object(self):
-        obj = get_object_or_404(Order, pub_id=self.kwargs.get('order_uuid'))
-        if obj.user==self.request.user:
-            return obj
-        else:
-            raise Http404()
+        obj = get_object_or_404(Order, pub_id=self.kwargs.get('order_uuid'), user=self.request.user)
+        return obj
             
     def get_serializer_class(self):
         if self.request.method == 'PUT':
@@ -49,13 +45,11 @@ class OrderItemList(generics.ListCreateAPIView):
         return OrderItemSerializer
     
     def get_queryset(self):
-        selected_order = get_object_or_404(Order, pub_id=self.kwargs.get('order_uuid'))
-        if selected_order.user!=self.request.user:
-            raise Http404()
+        selected_order = get_object_or_404(Order, pub_id=self.kwargs.get('order_uuid'), user=self.request.user)
         return OrderItem.objects.filter(order=selected_order)
         
     def perform_create(self, serializer):
-        selected_order=Order.objects.get(pub_id=self.kwargs['order_uuid'])
+        selected_order=get_object_or_404(Order, pub_id=self.kwargs.get('order_uuid'), user=self.request.user)
         serializer.save(order=selected_order)
 
 
@@ -68,8 +62,6 @@ class OrderItemDetail(generics.RetrieveUpdateDestroyAPIView):
         return OrderItemSerializer
 
     def get_object(self):
-        selected_order = get_object_or_404(Order, pub_id=self.kwargs.get('order_uuid'))
-        if selected_order.user!=self.request.user:
-            raise Http404()
+        selected_order = get_object_or_404(Order, pub_id=self.kwargs.get('order_uuid'), user=self.request.user)
         obj = get_object_or_404(selected_order.orderitems, pub_id=self.kwargs.get('orderitem_uuid'))
         return obj
