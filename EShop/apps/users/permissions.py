cd9cc17f4480ef99d0c2bff2af177149fcea7b4a
Andrii <baranovskyi.and@gmail.com>
2020-02-21 16:40:37 +0200

    hide ids from create views

--- EShop/apps/users/permissions.py
+++ EShop/apps/users/permissions.py
@@ -1,20 +1,16 @@
-from rest_framework.permissions import BasePermission
-from apps.users.models import UserProfile
 from django.contrib.auth.models import User
+from rest_framework.permissions import BasePermission
+from django.shortcuts import get_object_or_404
+
+from apps.users.models import UserProfile
 
 
 class UserProfileEditPermission(BasePermission):
     def has_permission(self, request, view):
-        if request.method != 'POST':
-            return False
-        try:
-            profile = UserProfile.objects.get(
-                uu_id=view.kwargs['user_profile_uu_id'],
-                user=request.user)
-            return profile.pk == request.data['user_profile']
-        except Exception:
-            print('No permission')
-            return False
+        profile = get_object_or_404(UserProfile,
+                                    uu_id=view.kwargs['user_profile_uu_id'],
+                                    user=request.user)
+        return True
 
 
 class RequestIsList(BasePermission):
--- EShop/apps/users/views.py
+++ EShop/apps/users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from apps.users.models import UserProfile, UserAddress, UserPhone
 from apps.users.serializers import UserProfileSerializer, UserAddressSerializer, UserPhoneSerializer
 from apps.users.permissions import UserProfileEditPermission, RequestIsList
@@ -38,6 +39,28 @@ class UserAddressList(generics.ListCreateAPIView):
     def get_queryset(self):
         return UserAddress.objects.filter(user_profile__user=self.request.user)
 
+    def create(request, *args, **kwargs):
+        profile = UserProfile.objects.get(
+            uu_id=kwargs['user_profile_uu_id'])
+        data = request.request.data
+        serializer = request.serializer_class(data=data)
+        if serializer.is_valid():
+            address = request.request.data['address']
+            city = request.request.data['city']
+            obj = UserAddress.objects.create(
+                city=city, address=address, user_profile=profile)
+            response_data = {
+                'address': obj.address,
+                'city': obj.city,
+                # 'uu_id': obj.serializable_value('uu_id'),
+            }
+            response_serializer = request.serializer_class(data=response_data)
+            if response_serializer.is_valid():
+                return Response(response_serializer.data,
+                                status=status.HTTP_201_CREATED)
+        return Response(response_serializer.errors,
+                        status=status.HTTP_400_BAD_REQUEST)
+
 
 class UserAddressDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = UserAddressSerializer
@@ -59,6 +82,27 @@ class UserPhoneList(generics.ListCreateAPIView):
 
     def get_queryset(self):
         return UserPhone.objects.filter(user_profile__user=self.request.user)
+
+    def create(request, *args, **kwargs):
+        profile = UserProfile.objects.get(
+            uu_id=kwargs['user_profile_uu_id'])
+        data = request.request.data
+        serializer = request.serializer_class(data=data)
+        if serializer.is_valid():
+            phone = request.request.data['phone']
+            obj = UserPhone.objects.create(
+                phone=phone, user_profile=profile)
+            response_data = {
+                'phone': obj.phone,
+                # 'uu_id': obj.serializable_value('uu_id'),
+            }
+            response_serializer = request.serializer_class(data=response_data)
+            if response_serializer.is_valid():
+                return Response(response_serializer.data,
+                                status=status.HTTP_201_CREATED)
+        return Response(response_serializer.errors,
+                        status=status.HTTP_400_BAD_REQUEST)
+        # return super(UserPhoneList, request).create(*args, **kwargs)
 
 
 class UserPhoneDetail(generics.RetrieveUpdateDestroyAPIView):
