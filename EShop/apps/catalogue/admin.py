23be5fab2252038207a3d297c556eda3e16023fd
Mary <slimmarkelova@gmail.com>
2020-02-24 12:44:34 +0200

    register catalogue to admin

--- EShop/apps/catalogue/admin.py
+++ EShop/apps/catalogue/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+
+class CatalogueAdmin(admin.ModelAdmin):
+    raw_id_fields = ("category",)
+
+
+admin.site.register(Category)
