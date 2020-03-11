12725aedd3731441208734af8fe95361d6a9b0d5
Alex shevelo <oshevelo@softserveinc.com>
2020-03-11 20:27:23 +0200

    fix tests

--- EShop/apps/shipments/serializers.py
+++ EShop/apps/shipments/serializers.py
@@ -1,22 +1,38 @@
 from rest_framework import serializers
 from apps.shipments.models import Shipment
 from apps.orders.serializers import OrderBriefSerializer
+from apps.orders.models import Order
+
 
 class ShipmentSerializer(serializers.ModelSerializer):
 
-    order = OrderBriefSerializer(read_only=True)    
+    order = OrderBriefSerializer()    
 
 
     class Meta:
         model = Shipment
         fields = ['uuid', 'order', 'shipment_status', 'shipment_type', 'shipment_date', 'destination_city', 'destination_zip_code', 'destination_adress_street', 'destination_adress_building', 'destination_other_details'
                  ]
-        def create(self, validated_data):
-            print('in create')
-            order_data = validated_data.pop['order']
-            validated_data.update({'order': Order.objects.filter(id=order_data['id']).first()})
-            shipment = Shipment.objects.create(**validated_data)
-            return shipment
+
+    def validate_order(self, value):
+        order = Order.objects.filter(id=value['id']).first()
+        if not order:
+            raise serializers.ValidationError("bae order id")
+        return value 
+        # todo: remove after relations feature release
+    #    profile = Profile.objects.filter(id=value).first()
+     #   if profile is None or (profile is not None and profile.allowed_relations is False):
+    # #       raise serializers.ValidationError("Profile is not allowed to add to relations")
+     #   return value
+
+    def create(self, validated_data):
+        print('in create')
+        print(validated_data)
+        order_data = validated_data.pop('order')
+        order = Order.objects.filter(id=order_data['id']).first()
+        validated_data.update({'order': order})
+        shipment = Shipment.objects.create(**validated_data)
+        return shipment
     '''
         perform_create
             1) order_data = json.pop['order']
