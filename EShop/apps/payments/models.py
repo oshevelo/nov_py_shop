789968394278593bd40cad6ee3d814acc02561af
Mariia997 <mariyamul997@gmail.com>
2020-03-11 10:55:07 +0200

    change model

--- EShop/apps/payments/models.py
+++ EShop/apps/payments/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from apps.orders.models import Order
+from django.contrib.postgres.fields import JSONField
 
 
 # Create your models here.
@@ -15,63 +16,19 @@ status = [
 class Payment(models.Model):
 
     id = models.AutoField(primary_key=True)
-
-    user = models.ForeignKey(
-	User, 
-	on_delete=models.CASCADE, 
-	null=True,  
-	blank=False
-    )
-
-    order = models.ForeignKey(
-	Order, 
-	on_delete=models.CASCADE, 
-	null=True, 
-	blank=False
-    )
-
-    amount = models.DecimalField(
-	default=0, 
-	max_digits=10, 
-	decimal_places=2, 
-	null=True, 
-	blank=False
-    )
-
-    creation_date = models.DateTimeField(
-	default=timezone.now, 
-	null=True, 
-	blank=False
-    )
-
-    complited_date = models.DateTimeField(
-	default=timezone.now, 
-	null=True, 
-	blank=False
-    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=False)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True, blank=False)
+    amount = models.DecimalField(default=0, max_digits=10, decimal_places=2, null=True, blank=False)
+    creation_date = models.DateTimeField(default=timezone.now, null=True, blank=False)
+    complited_date = models.DateTimeField(default=timezone.now, null=True, blank=False)
 
     def __str__(self):
-        return 'user = {}, public_id = {}, complited_date =     {}'.format(self.user, self.id, self.complited_date, self.pk)
+        return 'user = {}, public_id = {}, complited_date = {}'.format(self.user, self.id, self.complited_date, self.pk)
 
 
 class LogsTransaction(models.Model):
-    user = models.ForeignKey(
-	User, 
-	on_delete=models.CASCADE, 
-	null=True, 
-	blank=False
-    )
 
-    payment = models.ForeignKey(
-	Payment, 
-	on_delete=models.CASCADE, 
-	null=True, 
-	blank=False
-    )
-
-    status = models.CharField(
-	max_length=100, 
-	choices=status, 
-	null=True, 
-	blank=False
-    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=False)
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE, null=True, blank=False)
+    status = models.CharField(max_length=100, choices=status, null=True, blank=False)
+    data = JSONField(null=True, blank=False)
