d2df81b8bd3eceabbb2839d7489b5b2740d57f70
Andrii <baranovskyi.and@gmail.com>
2020-03-24 14:16:15 +0200

    typo

--- EShop/apps/stats/views.py
+++ EShop/apps/stats/views.py
@@ -6,7 +6,6 @@ from apps.stats.serializers import StatSerializer
 
 
 class StatCreate(generics.CreateAPIView):
-    pagination_class = LimitOffsetPagination
     permission_classes = (IsAuthenticated,)
     serializer_class = StatSerializer
 
